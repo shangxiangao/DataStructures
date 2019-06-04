9adbee08667263066281fc9e4454a1685ae99e68
shangxiangao <18120165591@163.com>
2019-06-04 14:52:20 +0800

    test

--- src/main/java/stack/PolandNotation.java
+++ src/main/java/stack/PolandNotation.java
@@ -2,6 +2,7 @@ package stack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PolandNotation {
     //Test result
@@ -17,11 +18,62 @@ public class PolandNotation {
         List<String> sl = new ArrayList<String>();
         int i = 0;
         String str = "";
-
+        char c = ' ';
+        while(i < s.length()){
+            //When s.charAt(i) is a operator or a bracket
+            if((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
+                sl.add("" + c);
+                i++;
+            }else{
+                //When s.charAt(i) is a number
+                str = "";
+                //Cannot separate a number larger than 9
+                while(i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57){
+                    str += c;
+                    i++;
+                }
+                sl.add("" + str);
+            }
+        }
+        return sl;
     }
-
+    //Transfer infix expressions to suffix expressions
     public static List<String> toSuffixExpression(List<String> sl) {
-
+        //1. Define two collections s1, s2
+        //For operators
+        Stack<String> s1 = new Stack<String>();
+        //For final result
+        List<String> s2 = new ArrayList<>();
+        //2. Scan the infix expression(left to right)
+        for (String item : sl) {
+            //If there is a number, put it in s2
+            if(item.matches("\\d+"))
+                s2.add(item);
+            //If there is a "(", put it in s1 directly
+            else if(item.equals("("))
+                s1.push(item);
+            //if the item is a ")", pop the items in s1 and adding them into s2 until the top of s1 is a "("
+            else if(item.equals(")")){
+                while (!s1.peek().equals("(")){
+                    s2.add(s1.pop());
+                }
+                s1.pop();
+            //If there is a operator
+            }else{
+            //(1) Compare the priority of the new item and the top item of s1
+                while(!s1.isEmpty() && Priority.getValue(item) < Priority.getValue(s1.peek())){
+                    //If the priority of the new item is smaller, continuously pop the items
+                    //in s1 and push them into s2 until the the priority of the new item is
+                    //larger or equals to the top item of s1
+                    s2.add(s1.pop());
+                }
+                s1.push(item);
+            }
+        }
+        while (!s1.empty()){
+            s2.add(s1.pop());
+        }
+        return s2;
     }
 }
 // Get the priority level of different operators
